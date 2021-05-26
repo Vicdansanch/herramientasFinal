a8d8f2b7affbfde612f7076b7d77745bd338c255
Vicdansanch <vicdansanch@gmail.com>
2021-05-25 19:08:41 -0500

    init

---
+++ src/main/java/intento2/main/modelos/horario.java
@@ -0,0 +1,41 @@
+
+public class Horario{
+    private int HorarioID;
+    private int HoraInicio;
+    private int HoraFin;
+    private String DiaDeLaSemana;
+
+public int getHorarioID() {
+	return this.HorarioID;
+}
+public void setHorarioID(int HorarioID) {
+	this.HorarioID = HorarioID;
+}
+
+
+    public int getHoraInicio() {
+    	return this.HoraInicio;
+    }
+    public void setHoraInicio(int HoraInicio) {
+    	this.HoraInicio = HoraInicio;
+    }
+
+
+    public int getHoraFin() {
+    	return this.HoraFin;
+    }
+    public void setHoraFin(int HoraFin) {
+    	this.HoraFin = HoraFin;
+    }
+
+
+    public String getDiaDeLaSemana() {
+    	return this.DiaDeLaSemana;
+    }
+    public void setDiaDeLaSemana(String DiaDeLaSemana) {
+    	this.DiaDeLaSemana = DiaDeLaSemana;
+    }
+
+    
+
+}
---
+++ src/main/java/intento2/main/modelos/materia.java
@@ -0,0 +1,93 @@
+
+public class Materia {
+    private int ID;
+    private String Secuencia;
+    private String Profesor;
+    private String Materia;
+    private String Turno;
+    private int HorarioLunes;
+    private int HorarioMartes;
+    private int HorarioMiercoles;
+    private int HorarioJueves;
+    private int HorarioViernes;
+
+    public int getID() {
+    	return this.ID;
+    }
+    public void setID(int ID) {
+    	this.ID = ID;
+    }
+
+
+    public String getSecuencia() {
+    	return this.Secuencia;
+    }
+    public void setSecuencia(String Secuencia) {
+    	this.Secuencia = Secuencia;
+    }
+
+
+    public String getProfesor() {
+    	return this.Profesor;
+    }
+    public void setProfesor(String Profesor) {
+    	this.Profesor = Profesor;
+    }
+
+
+    public String getMateria() {
+    	return this.Materia;
+    }
+    public void setMateria(String Materia) {
+    	this.Materia = Materia;
+    }
+
+
+    public String getTurno() {
+    	return this.Turno;
+    }
+    public void setTurno(String Turno) {
+    	this.Turno = Turno;
+    }
+
+
+    public int getHorarioLunes() {
+    	return this.HorarioLunes;
+    }
+    public void setHorarioLunes(int HorarioLunes) {
+    	this.HorarioLunes = HorarioLunes;
+    }
+
+
+    public int getHorarioMartes() {
+    	return this.HorarioMartes;
+    }
+    public void setHorarioMartes(int HorarioMartes) {
+    	this.HorarioMartes = HorarioMartes;
+    }
+
+
+    public int getHorarioMiercoles() {
+    	return this.HorarioMiercoles;
+    }
+    public void setHorarioMiercoles(int HorarioMiercoles) {
+    	this.HorarioMiercoles = HorarioMiercoles;
+    }
+
+
+    public int getHorarioJueves() {
+    	return this.HorarioJueves;
+    }
+    public void setHorarioJueves(int HorarioJueves) {
+    	this.HorarioJueves = HorarioJueves;
+    }
+
+
+    public int getHorarioViernes() {
+    	return this.HorarioViernes;
+    }
+    public void setHorarioViernes(int HorarioViernes) {
+    	this.HorarioViernes = HorarioViernes;
+    }
+
+}
